4d16d5782aab483f76018f554da1682d57989dbb
torreta <luiscampostorreta@gmail.com>
2017-04-06 01:38:12 +0000

    setting up test with rspec

---
+++ spec/requests/testing_classes_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "TestingClasses", type: :request do
+  describe "GET /testing_classes" do
+    it "works! (now write some real specs)" do
+      get testing_classes_index_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
