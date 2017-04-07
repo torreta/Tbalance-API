97ff1a5f8825ba28af1d8cb5090d1132fb6299cc
torreta <luiscampostorreta@gmail.com>
2017-04-07 03:22:26 +0000

    making 9 tests for fun 6 passing 3 pending, we will use this as guide for future testing

--- lib/zombie.rb
+++ lib/zombie.rb
@@ -1,8 +1,17 @@
 
 class Zombie
-   attr_accessor :name
+   attr_accessor :name, :brains, :alive, :rotting, :height
    
     def initialize
         @name = 'Ash'
+        @brains = 0
+        @alive = false
+        @rotting = true
+        @height = 6
     end
+    
+    #simple predicate
+    def hungry? 
+        true
+    end 
 end
--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 require "zombie"
 
+# config.expect_with(:rspec) { |c| c.syntax = :should }
+# config.raise_errors_for_deprecations!
 describe Zombie do
     #your examples (test) go here 
     
@@ -9,4 +11,49 @@ describe Zombie do
         zombie.name.should == 'Ash' #spectations, renames assertions
     end
     
+    it "has no brains" do
+        zombie = Zombie.new
+        zombie.brains.should be < 1 # should is the modifier of our test, < is the matcher
+    end
+    
+    it "zombie is dead" do
+        zombie = Zombie.new
+        zombie.alive.should be false #watch that sintax be_falsey works too
+        zombie.alive.should be_falsey
+        zombie.alive.should == false
+        
+    end
+    
+    it "is rotting" do
+        zombie = Zombie.new
+        zombie.rotting.should be true# should is the modifier of our test, < is the matcher
+        zombie.rotting.should == true
+        zombie.rotting.should be_truthy
+        
+    end
+    
+    it "is tall" do
+        zombie = Zombie.new
+        zombie.height.should be > 5# should is the modifier of our test, < is the matcher
+    end
+    
+    it "is hungry" do
+        zombie = Zombie.new
+        zombie.hungry?.should == true
+        zombie.should be_hungry
+    end
+    
+    it "too strong and fast" # this way you writte pending ones
+    
+    xit "has superpowers" do
+        #xit marks a pending test to make
+    
+    end
+    
+    it "are magical" do
+        pending # keyword works tooo, marks partially made tests
+        zombie = Zombie.new
+        zombie.magical?.should == true
+
+    end
 end
