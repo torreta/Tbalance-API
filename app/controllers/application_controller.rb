64d0d36f9c5f5e48e823cfcb7274c3c46a21b2c0
torreta <luiscampostorreta@gmail.com>
2017-04-11 02:51:52 +0000

    restarting some stuff and keeping some

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # 2 helpers brough from this tutorial 
+  # https://scotch.io/tutorials/build-a-restful-json-api-with-rails-5-part-one
+  include Response
+  include ExceptionHandler
 end
