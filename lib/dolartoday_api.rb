db19060df4262dbc5b46da315a236ab43d865c5b
torreta <luiscampostorreta@gmail.com>
2017-05-02 20:25:43 +0000

    added most methods for dotoday api, and all those tests

--- lib/dolartoday_api.rb
+++ lib/dolartoday_api.rb
@@ -48,6 +48,396 @@ class DolarTodayApi
         return Time.at(tiempo)
     end   
  
+    # "USD":
+    # transferencia
+    def USD_transfer
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:transferencia]
+        return value
+    end   
+
+    # transfer_cucuta
+    def USD_transfer_cucuta
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:transfer_cucuta]
+        return value
+    end   
+
+    # efectivo
+    def USD_efectivo
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:efectivo]
+        return value
+    end      
+    
+    # efectivo_real
+    def USD_efectivo_real
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:efectivo_real]
+        return value
+    end      
+    
+    # efectivo_cucuta
+    def USD_efectivo_real
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:efectivo_cucuta]
+        return value
+    end   
+    
+    # promedio
+    def USD_promedio
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:promedio]
+        return value
+    end   
+    
+    # promedio_real
+    def USD_promedio_real
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:promedio_real]
+        return value
+    end   
+    
+    # cencoex
+    def USD_cencoex
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:cencoex]
+        return value.to_f
+    end   
+    
+    # sicad1
+    def USD_sicad1
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:sicad1]
+        return value
+    end   
+    
+    # sicad2
+    def USD_sicad2
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:sicad2]
+        return value
+    end       
+    
+    # bitcoin_ref
+    def USD_bitcoin_ref
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:bitcoin_ref]
+        return value
+    end    
+    
+    # dolartoday
+    def USD_dolartoday
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USD][:dolartoday]
+        return value
+    end
+ 
+    # "EUR"
+    # transferencia
+    def EUR_transfer
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:transferencia]
+        return value
+    end
+    
+    # transfer_cucuta
+    def EUR_transfer_cucuta
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:transfer_cucuta]
+        return value
+    end
+    
+    # efectivo
+    def EUR_efectivo
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:efectivo]
+        return value
+    end
+    
+    # efectivo_real
+    def EUR_efectivo_real
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:efectivo_real]
+        return value
+    end
+    
+    # efectivo_cucuta
+    def EUR_efectivo_cucuta
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:efectivo_cucuta]
+        return value
+    end
+    
+    # promedio
+    def EUR_promedio
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:promedio]
+        return value
+    end   
+    
+    # promedio_real
+    def EUR_promedio_real
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:promedio_real]
+        return value
+    end   
+    
+    # cencoex
+    def EUR_cencoex
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:cencoex]
+        return value
+    end   
+    
+    # sicad1
+    def EUR_sicad1
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:sicad1]
+        return value
+    end   
+    
+    # sicad2
+    def EUR_sicad2
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:sicad2]
+        return value
+    end   
+    
+    # dolartoday
+    def EUR_dolartoday
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EUR][:dolartoday]
+        return value
+    end   
+
+    # COL
+    # efectivo
+    def COL_efectivo
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:COL][:efectivo]
+        return value
+    end   
+
+    # transfer
+    def COL_transfer
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:COL][:transfer]
+        return value
+    end   
+    
+    # compra
+    def COL_compra
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:COL][:compra]
+        return value
+    end   
+    
+    # venta
+    def COL_venta
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:COL][:venta]
+        return value
+    end   
+    
+    # GOLD
+    # rate
+    def GOLD
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:GOLD][:rate]
+        return value
+    end  
+    
+    # USDVEF
+    # rate
+    def USDVEF
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDVEF][:rate]
+        return value
+    end  
+    
+    # USDCOL
+    # setfxsell
+    def USDCOL_setfxsell
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDCOL][:setfxsell]
+        return value.to_f
+    end
+    
+    # setfxbuy
+    def USDCOL_setfxbuy
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDCOL][:setfxbuy]
+        return value.to_f
+    end
+    
+    # rate
+    def USDCOL_rate
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDCOL][:rate]
+        return value.to_f
+    end
+    
+    # ratecash
+    def USDCOL_ratecash
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDCOL][:ratecash]
+        return value.to_f
+    end
+    
+    # ratetrm
+    def USDCOL_ratetrm
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDCOL][:ratetrm]
+        return value.to_f
+    end
+    
+    # trmfactor
+    def USDCOL_trmfactor
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDCOL][:trmfactor]
+        return value.to_f
+    end
+    
+    # trmfactorcash
+    def USDCOL_trmfactorcash
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:USDCOL][:trmfactorcash]
+        return value
+    end
+    
+    # EURUSD
+    # rate
+    def EURUSD
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:EURUSD][:rate]
+        return value
+    end
+    
+    # BCV:
+    # fecha
+    def BCV_fecha
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        tiempo = body[:BCV][:fecha]
+        tiempo = DateTime.strptime(tiempo,'%s')
+        return Time.at(tiempo)
+    end
+    
+    # liquidez
+    def BCV_liquidez
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:BCV][:liquidez]
+        return value.to_f
+    end
+    
+    # reservas
+    def BCV_reservas
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:BCV][:reservas]
+        return value.to_f
+    end 
+
+    # MISC:
+    # petroleo
+    def MISC_petroleo
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:MISC][:petroleo]
+        return value.to_f
+    end
+    
+    # reservas
+     def MISC_reservas
+        uri = URI('http://api.bitcoinvenezuela.com/DolarToday.php?json=yes')
+        res = Net::HTTP.get_response(uri)
+        body = eval(res.body)
+        value = body[:MISC][:reservas]
+        return value.to_f
+    end
+ 
+ 
+ 
  
  
 end
--- spec/features/dolartoday_api_spec.rb
+++ spec/features/dolartoday_api_spec.rb
@@ -24,18 +24,226 @@ describe DolarTodayApi do
       expect(Time.at(server.hash_timestamp).is_a?(Time)).to be true
     end
 
-    it "data has USD value" do
+    # USD
+    it "USD_transfer present" do
       server = DolarTodayApi.new()
-      expect(Time.at(server.hash_timestamp).is_a?(Time)).to be true
+      expect(server.USD_transfer.is_a?(Float)).to be true
     end
-
+    
+    it "USD_transfer_cucuta present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_transfer_cucuta.is_a?(Float)).to be true
+    end
+    
+    it "USD_efectivo present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_efectivo.is_a?(Float)).to be true
+    end
+    
+    it "USD_efectivo_real present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_efectivo_real.is_a?(Float)).to be true
+    end
+    
+    it "USD_efectivo_real present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_efectivo_real.is_a?(Float)).to be true
+    end
+    
+    it "USD_promedio present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_promedio.is_a?(Float)).to be true
+    end
+    
+    it "USD_promedio_real present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_promedio_real.is_a?(Float)).to be true
+    end
+    
+    it "USD_cencoex present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_cencoex.is_a?(Float)).to be true
+    end
+    
+    it "USD_sicad1 present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_sicad1.is_a?(Float)).to be true
+    end
+    
+    it "USD_sicad2 present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_sicad2.is_a?(Float)).to be true
+    end
+    
+    it "USD_bitcoin_ref present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_bitcoin_ref.is_a?(Float)).to be true
+    end
+    
+    it "USD_dolartoday present" do
+      server = DolarTodayApi.new()
+      expect(server.USD_dolartoday.is_a?(Float)).to be true
+    end
+    
+    # EUR
+    it "EUR_transfer present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_transfer.is_a?(Float)).to be true
+    end
+    
+    it "EUR_transfer_cucuta present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_transfer_cucuta.is_a?(Float)).to be true
+    end
+    
+    it "EUR_efectivo present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_efectivo.is_a?(Float)).to be true
+    end
+    
+    it "EUR_efectivo_real present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_efectivo_real.is_a?(Float)).to be true
+    end
+    
+    it "EUR_efectivo_cucuta present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_efectivo_cucuta.is_a?(Float)).to be true
+    end
+    
+    it "EUR_promedio present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_promedio.is_a?(Float)).to be true
+    end
+    
+    it "EUR_promedio_real present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_promedio_real.is_a?(Float)).to be true
+    end
+    
+    it "EUR_cencoex present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_cencoex.is_a?(Float)).to be true
+    end
+    
+    it "EUR_sicad1 present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_sicad1.is_a?(Float)).to be true
+    end
+    
+    it "EUR_sicad2 present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_sicad2.is_a?(Float)).to be true
+    end
+    
+    it "EUR_dolartoday present" do
+      server = DolarTodayApi.new()
+      expect(server.EUR_dolartoday.is_a?(Float)).to be true
+    end
+    
+    # COL
+    it "COL_efectivo present" do
+      server = DolarTodayApi.new()
+      expect(server.COL_efectivo.is_a?(Float)).to be true
+    end
+    
+    it "COL_transfer present" do
+      server = DolarTodayApi.new()
+      expect(server.COL_transfer.is_a?(Float)).to be true
+    end
+    
+    it "COL_compra present" do
+      server = DolarTodayApi.new()
+      expect(server.COL_compra.is_a?(Float)).to be true
+    end
+    
+    it "COL_venta present" do
+      server = DolarTodayApi.new()
+      expect(server.COL_venta.is_a?(Float)).to be true
+    end
+    
+    # GOLD
+    it "GOLD present" do
+      server = DolarTodayApi.new()
+      expect(server.GOLD.is_a?(Float)).to be true
+    end
+    
+    # cambios
+    it "USDVEF present" do
+      server = DolarTodayApi.new()
+      expect(server.USDVEF.is_a?(Float)).to be true
+    end
+    
+    it "USDCOL_setfxsell present" do
+      server = DolarTodayApi.new()
+      expect(server.USDCOL_setfxsell.is_a?(Float)).to be true
+    end
+    
+    it "USDCOL_setfxbuy present" do
+      server = DolarTodayApi.new()
+      expect(server.USDCOL_setfxbuy.is_a?(Float)).to be true
+    end
+    
+    it "USDCOL_rate present" do
+      server = DolarTodayApi.new()
+      expect(server.USDCOL_rate.is_a?(Float)).to be true
+    end
+    
+    it "USDCOL_ratecash present" do
+      server = DolarTodayApi.new()
+      expect(server.USDCOL_ratecash.is_a?(Float)).to be true
+    end
+    
+    it "USDCOL_ratetrm present" do
+      server = DolarTodayApi.new()
+      expect(server.USDCOL_ratetrm.is_a?(Float)).to be true
+    end
+    
+    it "USDCOL_trmfactor present" do
+      server = DolarTodayApi.new()
+      expect(server.USDCOL_trmfactor.is_a?(Float)).to be true
+    end
+    
+    it "USDCOL_trmfactorcash present" do
+      server = DolarTodayApi.new()
+      expect(server.USDCOL_trmfactorcash.is_a?(Float)).to be true
+    end
+    
+    it "EURUSD present" do
+      server = DolarTodayApi.new()
+      expect(server.EURUSD.is_a?(Float)).to be true
+    end
+    
+    # BCV - Banco central de Venezuela
+    it "BCV_fecha present" do
+      server = DolarTodayApi.new()
+      expect(Time.at(server.BCV_fecha).is_a?(Time)).to be true
+    end
+    
+    it "BCV_liquidez present" do
+      server = DolarTodayApi.new()
+      expect(server.BCV_liquidez.is_a?(Float)).to be true
+    end
+    
+    it "BCV_reservas present" do
+      server = DolarTodayApi.new()
+      expect(server.BCV_reservas.is_a?(Float)).to be true
+    end
+    
+    # Micelaneous
+    it "MISC_petroleo present" do
+      server = DolarTodayApi.new()
+      expect(server.MISC_petroleo.is_a?(Float)).to be true
+    end
+    
+    it "MISC_petroleo present" do
+      server = DolarTodayApi.new()
+      expect(server.MISC_reservas.is_a?(Float)).to be true
+    end
 
 
     # should see if all important data is there
     it "hasnt changed its structure" 
-
-
-
 
 
     it "data is being inserted correctly"
